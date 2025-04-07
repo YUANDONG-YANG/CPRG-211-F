ee07c020ff39c3de4cc5c9845d9c3264ea934801
yuandong.yang <yuandong.yang@edu.sait.ca>
2025-04-06 23:12:02 -0600

    added  lab8 and assignment3

---
+++ CPRG211F/BasicMath.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicMath
+{
+    public class BasicMath
+    {
+        public double Add(double a, double b) => a + b;
+        public double Subtract(double a, double b) => a - b;
+        public double Multiply(double a, double b) => a * b;
+        public double Divide(double a, double b) => a / b; 
+    }
+}
