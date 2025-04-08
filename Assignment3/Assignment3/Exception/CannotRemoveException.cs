37f7bfba3047e3e94e9cb8902cee11fa492c8de8
yuandong.yang <yuandong.yang@edu.sait.ca>
2025-04-07 21:25:00 -0600

    added  lab8 and assignment3

---
+++ Assignment3/Assignment3/Exception/CannotRemoveException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Exception
+{
+    /// <summary>
+    /// Exception thrown when an item cannot be removed from a list
+    /// </summary>
+    [Serializable]
+    public class CannotRemoveException : System.Exception
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public CannotRemoveException() : base("Cannot remove item from the list") { }
+
+        /// <summary>
+        /// Constructor with custom message
+        /// </summary>
+        /// <param name="message">Custom error message</param>
+        public CannotRemoveException(string message) : base(message) { }
+
+        /// <summary>
+        /// Constructor with custom message and inner exception
+        /// </summary>
+        /// <param name="message">Custom error message</param>
+        /// <param name="innerException">Inner exception</param>
+        public CannotRemoveException(string message, System.Exception innerException)
+            : base(message, innerException) { }
+
+        /// <summary>
+        /// Serialization constructor
+        /// </summary>
+        /// <param name="info">Serialization information</param>
+        /// <param name="context">Streaming context</param>
+        protected CannotRemoveException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+    }
+}
---
+++ Assignment3/Assignment3/ProblemDomain/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assignment3.ProblemDomain
+{
+    /// <summary>
+    /// Internal node class for the linked list
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class Node
+    {
+        [DataMember]
+        public User Value { get; set; }
+
+        [DataMember]
+        public Node Next { get; set; }
+
+        public Node(User value)
+        {
+            Value = value;
+            Next = null;
+        }
+
+        // Empty constructor needed for deserialization
+        public Node()
+        {
+        }
+    }
+}
